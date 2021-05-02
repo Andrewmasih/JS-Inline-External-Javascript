15db47fb4857feae087457ba42452ae385975434
Andrew Masih <armasih@outlook.com>
2021-05-02 17:57:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+  /* EXTERNAL JAVASCRIPT, by 
+
+  External JavaScirpt by calling the JS file <script src = "filename.js">  </script> (or use the URL)   */   
+
+
+function () {
+  alert ("this is a alet box");
+  
+};
+
+console.log ("hey this works");
+
+
